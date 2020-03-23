9c5a6baff542df8e606d969bd04c1f80b6f457e3
Dmitrii Pron <idimzz@gmail.com>
2020-03-23 10:55:04 +0000

    move updates handler from listen to separate method

--- src/index.js
+++ src/index.js
@@ -85,6 +85,7 @@ export default class VkChatBot {
     this.schema = prepareSchema(schema)
     this.env = env
     this.processCommand = this.processCommand.bind(this)
+    this.updatesHandler = this.updatesHandler.bind(this)
     this.listen = this.listen.bind(this)
   }
 
@@ -180,6 +181,40 @@ export default class VkChatBot {
     })
   }
 
+  async updatesHandler ({
+    type,
+    group_id: groupId,
+    object
+  } = {}) {
+    switch (type) {
+      case 'confirmation':
+        return groupId && +groupId === +this.env.VK_GROUP_ID && this.env.VK_CHAT_BOT_CONFIRMATION_DATA
+      case 'message_new': {
+        const userId = object.message.peer_id || object.message.user_id
+        const storageDataResponse = await fetchPost(`https://api.vk.com/method/storage.get?user_id=${userId}&keys=bot_steps_history,bot_data&access_token=${this.env.VK_GROUP_TOKEN}&v=${this.env.VK_API_VERSION}&lang=${this.env.VK_LANG}`)
+        const storageData = (storageDataResponse.response || []).reduce((r, { key, value }) => ({ ...r, [key]: value }), {})
+        const stepsHistory = storageData.bot_steps_history
+          .split(',')
+          .filter(s => s)
+        const data = storageData.bot_data ? JSON.parse(storageData.bot_data) : {}
+        const payload = object.message.payload ? JSON.parse(object.message.payload) : {}
+        const command = payload.command || object.message.text || ''
+
+        await this.processCommand({
+          command,
+          stepsHistory,
+          data,
+          userId
+        })
+        break
+      }
+      default:
+        break
+    }
+
+    return 'ok'
+  }
+
   async listen (event = {}) {
     const response = {
       statusCode: 200,
@@ -192,34 +227,7 @@ export default class VkChatBot {
       const requestBody = JSON.parse(event.body)
 
       if (requestBody.secret === this.env.VK_CHAT_BOT_SECRET) {
-        switch (requestBody.type) {
-          case 'confirmation':
-            if (requestBody.group_id && +requestBody.group_id === +this.env.VK_GROUP_ID) {
-              response.body = this.env.VK_CHAT_BOT_CONFIRMATION_DATA
-            }
-            break
-          case 'message_new': {
-            const userId = requestBody.object.message.peer_id || requestBody.object.message.user_id
-            const storageDataResponse = await fetchPost(`https://api.vk.com/method/storage.get?user_id=${userId}&keys=bot_steps_history,bot_data&access_token=${this.env.VK_GROUP_TOKEN}&v=${this.env.VK_API_VERSION}&lang=${this.env.VK_LANG}`)
-            const storageData = (storageDataResponse.response || []).reduce((r, { key, value }) => ({ ...r, [key]: value }), {})
-            const stepsHistory = storageData.bot_steps_history
-              .split(',')
-              .filter(s => s)
-            const data = storageData.bot_data ? JSON.parse(storageData.bot_data) : {}
-            const payload = requestBody.object.message.payload ? JSON.parse(requestBody.object.message.payload) : {}
-            const command = payload.command || requestBody.object.message.text || ''
-
-            await this.processCommand({
-              command,
-              stepsHistory,
-              data,
-              userId
-            })
-            break
-          }
-          default:
-            break
-        }
+        response.body = await this.updatesHandler(requestBody)
 
         return response
       }
